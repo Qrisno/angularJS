e414bd80ea99ba8eea378cf397911af9ab3065bf
qrisno <62304291+Qrisno@users.noreply.github.com>
2021-04-06 00:18:02 +0400

    feat: angJS indexing, ng-repeat

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,7 +6,40 @@ let myModule = angular
             surname: 'ქრისტესიაშვილი',
             img: './images/cosmos-010.jpg'
         }
+        let dogs = [
+            {
+                name: 'chaki',
+                age:2
+            },
+            {
+                name: 'chaki1',
+                age:22
+            },
+            {
+                name: 'chaki3',
+                age:222
+            },
+        ]
+        let dogsChildren = [
+            {
+                name: 'chaki',
+                age:2,
+                children: ['ptra','shmuqa', 'ptra nino']
+            },
+            {
+                name: 'chaki1',
+                age:22,
+                children: ['ptra','shmuqa', 'ptra nino']
+            },
+            {
+                name: 'chaki3',
+                age:222,
+                children: ['ptra','shmuqa', 'ptra nino']
+            },
+        ]
         $scope.message = 'ჩემი პირველი კონტროლერი';
         $scope.employee = employee;
+        $scope.dogs = dogs;
+        $scope.dogsChildren = dogsChildren;
     });
 
